b910b2e65a62d804d65f0d75469d3ae7a0f93848
MaxFlower <10515741+MaxFlower@users.noreply.github.com>
2024-08-09 11:56:42 +1000

    feat: use context for information page to check display or hide modal

---
+++ src/app/information/layout.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Stack, Grid, GridItem, Heading } from '@chakra-ui/react'
+import { AppContextProvider } from '../../hooks/context.hook'
+import { ReactNode } from 'react'
+
+export default function InformationLayout({ children }: { children: ReactNode }) {
+    return (
+        <AppContextProvider>
+            <Grid
+                templateAreas={`"header" "main" "footer"`}
+                gridTemplateRows={'1fr 10fr 0.5fr'}
+                h='100vh'
+                gap='1'
+            >
+                <GridItem p='4' bg='green.50' area={'header'}>
+                    Menu
+                </GridItem>
+                <GridItem p='4' bg='grey.100' area={'main'} height='100%' overflow='scroll'>
+                    {children}
+                </GridItem>
+                <GridItem p='4' bg='blue.100' area={'footer'}>
+                    <Stack>
+                        <Heading as='h5' size='sm'>
+                            (c) {new Date().getFullYear()} Maksim Tsvetkov 🪴
+                        </Heading>
+                    </Stack>
+                </GridItem>
+            </Grid>
+        </AppContextProvider>
+    )
+}
--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -1,5 +1,20 @@
 'use client';
 
+import { useEffect, useState } from 'react'
+import { useAppContext } from '../../hooks/context.hook'
+import MultifunctionalModal from '../../components/multifunctional-modal'
+
 export default function InformationPage() {
-    return 'InformationPage'
+    const { profile, updateProfile } = useAppContext()
+    const [isIdentified, setIsIdentified] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsIdentified(profile !== null && profile?.username !== '' && profile?.jobTitle !== '')
+    }, [profile])
+
+    return (
+        <MultifunctionalModal title='Identify yourself' opened={!isIdentified} isProtected={true}>
+            test
+        </MultifunctionalModal>
+    )
 }
